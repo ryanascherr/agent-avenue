709675ee37030a9680b8ec4568b81728fc51b277
ryanascherr <ryanascherr@gmail.com>
2025-07-18 13:54:03 -0400

    Initiated project. Made game board.

---
+++ script.js
@@ -0,0 +1,57 @@
+let agents = [
+    {
+        name: "Double Agent",
+        quantity: 6,
+        one: -1,
+        two: 6,
+        three: -1
+    },
+    {
+        name: "Codebreaker",
+        quantity: 6,
+        one: 0,
+        two: 0,
+        three: "Win"
+    },
+    {
+        name: "Daredevil",
+        quantity: 6,
+        one: 2,
+        two: 3,
+        three: "Lose" 
+    },
+    {
+        name: "Enforcer",
+        quantity: 6,
+        one: 1,
+        two: 2,
+        three: 3
+    },
+    {
+        name: "Mole",
+        quantity: 1,
+        one: -3,
+    },
+    {
+        name: "Saboteur",
+        quantity: 6,
+        one: -1,
+        two: -1,
+        three: -2
+    },
+    {
+        name: "Sentinel",
+        quantity: 6,
+        one: 0,
+        two: 2,
+        three: 6
+    },
+    {
+        name: "Sidekick",
+        quantity: 1,
+        one: 4,
+
+    }
+]
+
+console.log(agents[0].name);
