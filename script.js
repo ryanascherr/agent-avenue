2f3bd7570ba4b6888bea308ae92a7b612ceca430
ryanascherr <ryanascherr@gmail.com>
2025-07-19 16:56:58 -0400

    Can now select two cards to submit, and added hand refill functionality.

--- script.js
+++ script.js
@@ -75,38 +75,54 @@ let computerSpace = 8;
 
 makeDeck();
 function makeDeck() {
+    console.log("---MAKING DECK---");
     agents.forEach(function(agent, index) {
         for (let i = 0; i < agent.quantity; i++) {
             deck.push(agent.name);
         }
     });
 
+    console.log(deck);
+
     document.querySelector(".js_cards-left").textContent = deck.length;
 }
 
+dealStartingHandsBtn.addEventListener('click', () => {
+    dealCards(human, 4);
+    dealCards(computer, 4);
+
+    displayHand(human);
+});
+
 function dealCards(player, numberOfCards) {
+    console.log("---DEALING CARDS---");
+    console.log(player.name + " is drawing " + numberOfCards + " cards.");
     let hand = player.hand;
 
     for (let i = 0; i < numberOfCards; i++) {
         let randomNumber = Math.floor(Math.random() * deck.length);
         hand.push(deck[randomNumber]);
+        console.log(player.name + " drew a " + deck[randomNumber]);
         deck.splice(randomNumber, 1);
     }
 
     document.querySelector(".js_cards-left").textContent = deck.length;
 
-    console.log(player.name + " drew " + numberOfCards + " cards. " + player.name + "'s hand is now:");
     console.log(player.hand);
+    console.log(deck);
 }
 
 function displayHand(player) {
+    console.log("---UPDATING HANDS---");
     let handElem = document.querySelector(".js_player-hand");
     let hand = player.hand;
 
+    console.log("-Emptying hands-");
     handElem.innerHTML = "";
     faceUpSelect.innerHTML = "";
     faceDownSelect.innerHTML = "";
 
+    console.log("-Appending cards-");
     for (let i = 0; i < hand.length; i++) {
         let agentName = hand[i];
         agentName = agentName.toLowerCase();
@@ -120,10 +136,74 @@ function displayHand(player) {
 
         let option = document.createElement('option');
         option.setAttribute('value', hand[i]);
+        option.setAttribute('data-index', i);
         option.innerText = hand[i];
+
+        let optionCopy = document.createElement('option');
+        optionCopy.setAttribute('value', hand[i]);
+        optionCopy.setAttribute('data-index', i);
+        optionCopy.innerText = hand[i];
+
         faceUpSelect.append(option);
-        faceDownSelect.append(option);
+        faceDownSelect.append(optionCopy);
     }
+}
+
+submitBtn.addEventListener('click', () => {
+    console.log("---SUBMITTING CARDS---");
+
+    let faceUpElem = faceUpSelect.selectedOptions[0];
+    let faceUpCardIndex = parseInt(faceUpElem.getAttribute('data-index'));
+    let faceUpCardName = faceUpSelect.value;
+
+    let faceDownElem = faceDownSelect.selectedOptions[0];
+    let faceDownCardIndex = parseInt(faceDownElem.getAttribute('data-index'));
+    let faceDownCardName = faceDownSelect.value;
+
+    console.log(human.name + "'s hand is:");
+    console.log(human.hand);
+    console.log("Face-up card is " + faceUpCardName);
+    console.log("Face-down card is " + faceDownCardName);
+
+    if (faceUpCardName === faceDownCardName) {
+        console.log("Can't choose the same two cards.");
+    } else {
+        removeCardsFromHand(human, faceUpCardIndex, faceDownCardIndex);
+        dealCards(human, 2);
+        displayHand(human);
+    }
+});
+
+function removeCardsFromHand(player, faceUpCardIndex, faceDownCardIndex) {
+    if (faceUpCardIndex > faceDownCardIndex) {
+        player.hand.splice(faceUpCardIndex, 1);
+        player.hand.splice(faceDownCardIndex, 1);
+    }
+    if (faceDownCardIndex > faceUpCardIndex) {
+        player.hand.splice(faceDownCardIndex, 1);
+        player.hand.splice(faceUpCardIndex, 1);
+    }
+
+    // player.hand.forEach(function(agent, index) {
+    //     for (let i = 0; i < player.hand.length; i++) {
+    //         if (agent === cardOne) {
+    //             console.log("Removing " + player.hand[i] + " from hand.");
+    //             player.hand.splice(i, 1);
+    //             break;
+    //         }
+    //     }
+    // });
+
+    // player.hand.forEach(function(agent, index) {
+    //     for (let i = 0; i < player.hand.length; i++) {
+    //         if (agent === cardTwo) {
+    //             console.log("Removing " + player.hand[i] + " from hand.");
+    //             player.hand.splice(i, 1);
+    //             break;
+    //         }
+    //     }
+    // });
+    
 }
 
 function moveSpaces(player, number) {
@@ -141,14 +221,3 @@ function moveSpaces(player, number) {
 
     player.space = newSpace;
 }
-
-dealStartingHandsBtn.addEventListener('click', () => {
-    dealCards(human, 4);
-    dealCards(computer, 4);
-
-    displayHand(human);
-});
-
-submitBtn.addEventListener('click', () => {
-    
-});
